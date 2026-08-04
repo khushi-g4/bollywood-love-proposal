d9f0417951e37f494230c2fe8debe6624492d9b4
Khushi Goel <khushigoel.04g06@gmail.com>
2026-08-05 01:40:36 +0530

    size

--- src/components/Scene/Moon.tsx
+++ src/components/Scene/Moon.tsx
@@ -1,13 +1,25 @@
 import { Sphere } from "@react-three/drei";
+import { Float } from "@react-three/drei";
 
 export default function Moon() {
   return (
-    <Sphere args={[1, 64, 64]} position={[3, 1.5, 0]}>
-      <meshStandardMaterial
-        color="#f8f8f8"
-        emissive="#dbeafe"
-        emissiveIntensity={0.25}
-      />
-    </Sphere>
+    <Float
+      speed={1}
+      rotationIntensity={0.15}
+      floatIntensity={0.5}
+    >
+      <Sphere
+        args={[0.65, 64, 64]}
+        position={[4.8, 2.6, 0]}
+      >
+        <meshStandardMaterial
+          color="#f8f8f8"
+          emissive="#dbeafe"
+          emissiveIntensity={0.35}
+          roughness={0.9}
+          metalness={0}
+        />
+      </Sphere>
+    </Float>
   );
 }
