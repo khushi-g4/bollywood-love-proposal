405d49a920949666d1d5c0032c19febf884be6ad
Khushi Goel <khushigoel.04g06@gmail.com>
2026-08-05 01:26:40 +0530

    slight change

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -3,7 +3,7 @@ import Stars from "../Stars/Stars";
 
 export default function Background() {
   return (
-    <div className="fixed inset-0 -z-50 overflow-hidden bg-slate-950">
+<div className="fixed inset-0 z-0 overflow-hidden bg-slate-950">
 
       <Stars />
 
