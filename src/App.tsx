ca41375c13a4339eeb91922f1f23b7bfcb83d94b
Khushi Goel <khushigoel.04g06@gmail.com>
2026-08-05 01:16:23 +0530

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Layout from "./components/Layout/Layout";
+import Background from "./components/Background/Background";
 
 function App() {
   return (
     <Layout>
+      <Background />
+
       <div className="flex h-screen items-center justify-center">
         <h1 className="text-6xl font-bold text-pink-400">
           Forever Begins Today ❤️
---
+++ src/components/Background/Background.tsx
@@ -0,0 +1,5 @@
+export default function Background() {
+  return (
+    <div className="fixed inset-0 -z-50 bg-slate-950" />
+  );
+}
