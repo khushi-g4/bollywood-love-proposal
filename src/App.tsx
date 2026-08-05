5fee8e5017db032508d380784805d57cd2ea4400
Khushi Goel <khushigoel.04g06@gmail.com>
2026-08-06 02:28:10 +0530

    folder manage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="w-screen h-screen bg-slate-950 flex items-center justify-center">
+    <div className="w-screen h-screen bg-[#020617] flex items-center justify-center">
       <h1 className="text-pink-400 text-6xl font-bold">
         Forever Begins Today ❤️
       </h1>
