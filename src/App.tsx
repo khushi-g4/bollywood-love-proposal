25f4440773f3f78eca75e0e0a4f5d4ded8f89dd3
Khushi Goel <khushigoel.04g06@gmail.com>
2026-08-06 02:08:46 +0530

    u[pdate

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import Layout from "./components/Layout/Layout";
 import Background from "./components/Background/Background";
+import Scene1 from "./components/Scene1/Scene1";
 
 function App() {
   return (
     <Layout>
       <Background />
-
-      <div className="relative z-10 flex h-screen items-center justify-center">
-        <h1 className="text-6xl font-bold text-pink-400">
-          Forever Begins Today ❤️
-        </h1>
-      </div>
+      <Scene1 />
     </Layout>
   );
 }
---
+++ src/components/Scene1/Scene1.tsx
@@ -0,0 +1,9 @@
+import BlackScreen from "./BlackScreen";
+
+export default function Scene1() {
+  return (
+    <>
+      <BlackScreen />
+    </>
+  );
+}
