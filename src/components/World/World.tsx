4091cb7c0c5000a5fd1971c8efbc009c04c388f0
Khushi Goel <khushigoel.04g06@gmail.com>
2026-08-06 02:35:01 +0530

    stars

--- src/components/World/World.tsx
+++ src/components/World/World.tsx
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import Sky from "./Sky";
+import StarField from "./Stars";
 
 export default function World() {
   return (
@@ -13,6 +14,18 @@ export default function World() {
       >
         <Suspense fallback={null}>
           <Sky />
+          <Suspense fallback={null}>
+  <Sky />
+
+  <StarField />
+
+  <ambientLight intensity={0.35} />
+
+  <directionalLight
+    position={[5, 5, 5]}
+    intensity={1.5}
+  />
+</Suspense>
 
           <ambientLight intensity={0.35} />
 
